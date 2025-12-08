c8a964a0af1f67e2ad7e71be67370b8f1d48d0d3
priyamore2018 <piya.more2018@gmail.com>
2025-12-08 22:27:57 +0530

    ITVedantFirstProject_18Nov2025_test

---
+++ eclipse-workspace/ITVedantFirrstProject_18Nov2025/src/FirstProgram/FirstBasicJAvaProgram.java
@@ -0,0 +1,15 @@
+package FirstProgram;
+
+public class FirstBasicJAvaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for (int  x = 1 ; x<=5 ; x ++)
+		{
+		System. out.println(x);
+		}
+	}
+
+}
